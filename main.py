13a62548222db2766cc966a15b4176de7141856e
lela null <lela.null@users.noreply.github.com>
2022-07-15 19:19:16 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+from analytics import statistics
+print("You may have to run this file a few times.")
+print("Each time you run this file it will give you instructions to complete setup.")
+statistics
+
+
